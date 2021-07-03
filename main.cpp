26870cea7afee136edf0ac176ceeb8c502cbeaf8
Robert Westman <robert@byteflux.io>
2021-07-03 18:05:17 +0200

    Switch to dynamically linked

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <QApplication>
+#include <QWindow>
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
+    QWindow window;
+    window.show();
     std::cout << "Running." << std::endl;
-    return 0;
+    return QApplication::exec();
 }
