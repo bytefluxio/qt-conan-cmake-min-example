591573c7d92764cf9a11b3f72a6574dfebc52073
Robert Westman <robert@byteflux.io>
2021-07-03 16:32:31 +0200

    Add use of QApplication

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <QApplication>
 
 int main(int argc, char *argv[]) {
+    QApplication application(argc, argv);
     std::cout << "Running." << std::endl;
     return 0;
 }
